8f9191566d6553402a6de2109bc67350e6e85a1e
Amber Joy <amber.joy.dev@gmail.com>
2023-02-15 11:32:18 -0600

    add css
    Now let's get creative! Please apply some style with CSS. Make it fun!

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Serve static files from the public directory
+app.use(express.static("public"));
+
 // Endpoint to return an HTML unordered list of cat names with attributes from a JSON file
 app.get("/cats", (req, res) => {
   // Load the JSON file with cat data
   const catData = require("./cat-names.json");
 
   // Generate an HTML unordered list with sub-lists for each cat's attributes
-  let html = "<ul>";
+  let html = '<ul class="cat-list">';
   catData.forEach(cat => {
-    html += `<li>${cat.name}<ul><li>Size: ${cat.size}</li><li>Color: ${cat.color}</li></ul></li>`;
+    html += `<li class="cat-item">${cat.name}<ul class="cat-attributes"><li>Size: ${cat.size}</li><li>Color: ${cat.color}</li></ul></li>`;
   });
   html += "</ul>";
 

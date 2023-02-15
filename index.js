9ab54e71f532b0a4af697946f612d850806c24ec
Amber Joy <amber.joy.dev@gmail.com>
2023-02-15 11:30:40 -0600

    list the cats with html
    now use the response of that endpoint to render an html unordered list of the cat names

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Endpoint to return a list of cat names from a JSON file
+// Endpoint to return an HTML unordered list of cat names from a JSON file
 app.get("/cats", (req, res) => {
   // Load the JSON file with cat names
   const catNames = require("./cat-names.json");
-  res.json(catNames);
+
+  // Generate an HTML unordered list of the cat names
+  let html = "<ul>";
+  catNames.forEach(name => {
+    html += `<li>${name}</li>`;
+  });
+  html += "</ul>";
+
+  // Send the HTML back to the client as a response
+  res.send(html);
 });
 
 // Start the server
